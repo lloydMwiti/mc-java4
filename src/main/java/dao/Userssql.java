cfd4475a857dbea304a100b2a58516d851945ebf
lloydM <lloydMwiti4@gmail.com>
2020-07-25 22:05:58 +0300

    adds connection files

---
+++ src/main/java/dao/Userssql.java
@@ -0,0 +1,22 @@
+package dao;
+
+import models.Department;
+
+import java.util.List;
+
+public class Departmentsql implements DepartmentDao {
+    @Override
+    public List<Department> all() {
+        return null;
+    }
+
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void find(int id) {
+
+    }
+}

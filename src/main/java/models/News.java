05ae74d76af22cda65a8d5a152ffd90a6cfe97cb
lloydM <lloydMwiti4@gmail.com>
2020-07-25 23:54:10 +0300

    adds news model

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,35 @@
 package models;
 
 public class News {
+    private int id;
+    private String news,department;
+
+    public News(String news, String department){
+        this.news=news;
+        this.department=department;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNews(String news) {
+        this.news = news;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNews() {
+        return news;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
 }

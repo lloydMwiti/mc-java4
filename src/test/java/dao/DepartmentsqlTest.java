f1eab437aad596ebb0b49ee38d83dacdc548d171
lloydM <lloydMwiti4@gmail.com>
2020-07-26 00:03:25 +0300

    generates dep tests

---
+++ src/test/java/dao/DepartmentsqlTest.java
@@ -0,0 +1,15 @@
+package dao;
+
+import junit.framework.TestCase;
+
+public class DepartmentsqlTest extends TestCase {
+
+    public void testAll() {
+    }
+
+    public void testAdd() {
+    }
+
+    public void testFind() {
+    }
+}

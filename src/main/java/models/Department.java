2a311324fba43630f176df631c3533584ad7b417
lloydM <lloydMwiti4@gmail.com>
2020-07-25 23:48:57 +0300

    adds department model

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,5 +1,26 @@
 package models;
 
 public class Department {
-    
+    private int id;
+    private String name;
+
+    public Department(String name){
+        this.name=name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }

5002a90c7a1c3bd20683d5f79dbcb65777a04381
lloydM <lloydMwiti4@gmail.com>
2020-07-26 00:02:42 +0300

    adds newssql tests template

---
+++ src/test/java/dao/NewssqlTest.java
@@ -0,0 +1,15 @@
+package dao;
+
+import junit.framework.TestCase;
+
+public class NewssqlTest extends TestCase {
+
+    public void testAll() {
+    }
+
+    public void testAdd() {
+    }
+
+    public void testFind() {
+    }
+}
